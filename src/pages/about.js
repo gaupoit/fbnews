827852348a69b76554ed46a5047b50d948f9eec7
Thinh Nguyen <thinhnp@ymese.com>
2020-08-11 10:23:00 +0700

    Init site

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../components/header";
+
+export default function About() {
+    return (
+        <div style={{ color: 'purple' }}>
+            <Header headerText="About Socail News" />
+            <p>Say WOW. Very useful.</p>
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
-import React from "react"
+import React from "react";
+import { Link } from "gatsby";
+import Header from "../components/header";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: "#ff6300"}}>
+      <Link to="/about/">About</Link>
+      <Header headerText="Socail News" />
+      <p>Most quality news</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  )
 }
